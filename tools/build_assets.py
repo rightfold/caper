184512b24cb3bbeaadb2a3c8010f69de1ed03654
rightfold <rightfold@gmail.com>
2017-10-31 20:56:18 +0100

    [font] Add font

--- tools/build_assets.py
+++ tools/build_assets.py
@@ -15,12 +15,24 @@ def build_blend(blend_file, obj_path):
     ]
     subprocess.run(command, check=True)
 
+def build_xcf(xcf_file, png_file):
+    command = [
+        'xcf2png',
+        xcf_file,
+        '-o', png_file,
+    ]
+    subprocess.run(command, check=True)
+
 def main():
     target_dir = os.environ['OUT_DIR']
-    for blend_file in iglob('src/**/*.blend', recursive=True):
-        obj_path = target_dir + '/' + blend_file[4:-6] + '.obj'
+    for blend_path in iglob('src/**/*.blend', recursive=True):
+        obj_path = target_dir + '/' + blend_path[4:-6] + '.obj'
         os.makedirs(os.path.dirname(obj_path), exist_ok=True)
-        build_blend(blend_file, obj_path)
+        build_blend(blend_path, obj_path)
+    for xcf_path in iglob('src/**/*.xcf', recursive=True):
+        png_path = target_dir + '/' + xcf_path[4:-4] + '.png'
+        os.makedirs(os.path.dirname(png_path), exist_ok=True)
+        build_xcf(xcf_path, png_path)
 
 if __name__ == '__main__':
     main()
