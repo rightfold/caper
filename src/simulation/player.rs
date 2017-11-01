d32fa7b090b720a39f453458e28d8fd26b213a3d
rightfold <rightfold@gmail.com>
2017-11-01 19:56:54 +0100

    [player] [gas_spores] Make gas spores attackable

--- src/simulation/player.rs
+++ src/simulation/player.rs
@@ -4,6 +4,7 @@ use rand::Rng;
 use chance::gen_range_base;
 use input::Input;
 use world::World;
+use world::effect::damage_indicator::DamageIndicatorSet;
 use world::player::*;
 
 const MOVEMENT_SPEED: f32 = 4.0;
@@ -15,11 +16,11 @@ const ATTACK_KNOCK_BACK: f32 = 3.0;
 
 pub fn simulate<R: Rng>(world: &mut World, input: &Input, rng: &mut R,
                         dt: f32) {
-    simulate_move(&mut world.player, input, dt);
-    simulate_attack_state(world, input, rng, dt);
+    simulate_move(dt, input, &mut world.player);
+    simulate_attack_state(rng, dt, input, world);
 }
 
-fn simulate_move(player: &mut Player, input: &Input, dt: f32) {
+fn simulate_move(dt: f32, input: &Input, player: &mut Player) {
     let dposition = MOVEMENT_SPEED * dt * Vector2::new(
         -(input.move_west  as i32 as f32) + (input.move_east  as i32 as f32),
         (input.move_north as i32 as f32) - (input.move_south as i32 as f32),
@@ -30,8 +31,8 @@ fn simulate_move(player: &mut Player, input: &Input, dt: f32) {
     }
 }
 
-fn simulate_attack_state<R: Rng>(world: &mut World, input: &Input, rng: &mut R,
-                                 dt: f32) {
+fn simulate_attack_state<R: Rng>(rng: &mut R, dt: f32, input: &Input,
+                                 world: &mut World) {
     if !input.attack {
         world.player.holding_attack = false;
     }
@@ -40,7 +41,7 @@ fn simulate_attack_state<R: Rng>(world: &mut World, input: &Input, rng: &mut R,
             if input.attack && !world.player.holding_attack {
                 world.player.attack_state = AttackState::Attacking(0.0);
                 world.player.holding_attack = true;
-                simulate_attack(world, rng);
+                simulate_attack(rng, world);
             },
         AttackState::Attacking(since) if since > 1.0 =>
             world.player.attack_state = AttackState::NotAttacking,
@@ -49,18 +50,35 @@ fn simulate_attack_state<R: Rng>(world: &mut World, input: &Input, rng: &mut R,
     }
 }
 
-fn simulate_attack<R: Rng>(world: &mut World, rng: &mut R) {
-    let velocities = soa_array!(world.spiders, mut velocities).iter_mut();
-    let positions  = soa_array!(world.spiders, positions).iter();
-    let healths    = soa_array!(world.spiders, mut healths).iter_mut();
-    for (velocity, (position, health)) in velocities.zip(positions.zip(healths)) {
-        if world.player.position.distance(*position) < ATTACK_RANGE {
+fn simulate_attack<R: Rng>(rng: &mut R, world: &mut World) {
+    simulate_attack_impl(
+        rng, &world.player, &mut world.damage_indicators,
+        soa_array!(world.spiders, mut healths),
+        soa_array!(world.spiders, positions),
+        soa_array!(world.spiders, mut velocities),
+    );
+    simulate_attack_impl(
+        rng, &world.player, &mut world.damage_indicators,
+        soa_array!(world.gas_spores, mut healths),
+        soa_array!(world.gas_spores, positions),
+        soa_array!(world.gas_spores, mut velocities),
+    );
+}
+
+fn simulate_attack_impl<R: Rng>(rng: &mut R, player: &Player,
+                                damage_indicators: &mut DamageIndicatorSet,
+                                healths: &mut [f32], positions: &[Vector2<f32>],
+                                velocities: &mut [Vector2<f32>]) {
+    let monsters =
+        healths.iter_mut().zip(positions.iter().zip(velocities.iter_mut()));
+    for (health, (position, velocity)) in monsters {
+        if player.position.distance(*position) < ATTACK_RANGE {
             let damage = gen_range_base(rng, ATTACK_DAMAGE_BASE,
                                         ATTACK_DAMAGE_RANGE);
             *health -= damage;
-            world.damage_indicators.spawn(*position, damage);
+            damage_indicators.spawn(*position, damage);
 
-            *velocity += (*position - world.player.position)
+            *velocity += (*position - player.position)
                 .normalize_to(ATTACK_KNOCK_BACK);
         }
     }
